e5ab881e57f9ab61c6261a08be48282d50a8e783
stguarin <sguarina@unal.edu.co>
2024-11-25 21:33:00 -0500

    Update

--- descuentos.py
+++ descuentos.py
@@ -1,11 +1,14 @@
-
+# primero mostramos el menu en pantalla
 print('Menu\n1:     Haburguesa      $10.000\n2:     Papas fritas    $6.000\n3:     Bebida          $4.000')
+# predefinimos variables para usar posteriormente, y evitar problemas en el codigo mas adelante
 terminar = 'no'
 costo_H = 0
 costo_P = 0 
 costo_B = 0
+#cree un bucle para solicitar la orden, si prefieres lo puedo dejar para que pida la orden de forma completa en lugar de usar el bucle
 while terminar != 'si':
     orden = int(input('dijite el indice de la comida que desea solicitar '))
+    #confirmo si el valor del indice coincide con los del menu
     if orden >= 1 and orden <= 3:
         if orden == 1:
             hamburguesas = int(input('ingrese la cantidad de hamburguesas de desea comprar '))
@@ -18,12 +21,8 @@ while terminar != 'si':
             costo_B = bebidas * 4000
     else:
         print('por favor dijite un indice valido para la orden')
+        #confirmacion para romper el bucle
     terminar = input('desea finalizar la orden ? si/no ')
 total = costo_H+costo_B+costo_P
-factura_sin_descuento ={
-    'Hamburguesa' : costo_H,
-    'Papas fritas' : costo_P,
-    'bebida' : costo_B,
-    'total' : total
-}
+#impresion de una factura prelliminar sin descuentos 
 print(f'el valor total de la factura sin descuento es de \nHaburguesa      ${costo_H}\nPapas fritas    ${costo_P}\nBebida          ${costo_B}')
